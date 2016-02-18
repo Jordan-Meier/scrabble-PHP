85b049afdb49bc451861f9d47bc15b9df3139298
Jordan Meier and Jon Nardine <jwnardine@gmail.com>
2016-02-18 09:44:12 -0800

    add a route to score results

--- app/app.php
+++ app/app.php
@@ -11,5 +11,10 @@
         return $app['twig']->render('index.html.twig');
     });
 
+    $app->get("/scrabble_score", function() use ($app) {
+        $my_Scrabble = new Scrabble;
+        $scrabble_score = $my_Scrabble->findScore($_GET['word']);
+        return $app['twig']->render('index.html.twig', array('score' => $scrabble_score));
+    });
     return $app;
 ?>
